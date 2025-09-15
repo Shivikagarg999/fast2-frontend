c46d167ac19af73480f0bded0cb9fb05bd3b1840
shivikagarg999 <shivikagarg91@gmail.com>
2025-09-15 11:12:46 +0530

    kjf

--- src/app/components/productCard/page.jsx
+++ src/app/components/productCard/page.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from 'react';
 import { PlusIcon, MinusIcon } from '@heroicons/react/24/outline';
 import { StarIcon as StarSolidIcon } from '@heroicons/react/24/solid';
